c1bdbf7595104f5eab592ef2df8f836f233d9f79
knix2000 <ulflind@kth.se>
2020-07-03 22:25:43 +0200

    Updates

---
+++ precache-manifest.abe19887ee47f65c2850c3d21d5ab3e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63a9ebda710e55d54508e0e9609e92e",
+    "url": "/delpht/index.html"
+  },
+  {
+    "revision": "45285813aad17e48217c",
+    "url": "/delpht/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f4d57d7b072d95edabd",
+    "url": "/delpht/static/js/2.9d985579.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/delpht/static/js/2.9d985579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45285813aad17e48217c",
+    "url": "/delpht/static/js/main.30dec592.chunk.js"
+  },
+  {
+    "revision": "d8883df636c72aec475d",
+    "url": "/delpht/static/js/runtime-main.6defaa82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/delpht/static/media/logo.5d5d9eef.svg"
+  }
+]);
